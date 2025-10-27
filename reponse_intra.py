07434a5ab0f6b053e38ecc35d29451dd7c9360f2
6240890 <6240890@etu.cegepsth.qc.ca>
2025-10-27 12:28:55 -0400

    fin exam

--- reponse_intra.py
+++ reponse_intra.py
@@ -10,6 +10,7 @@ import numpy as np
 
 def question1():
    print("Question 1 : Température du terrarium")
+   print()
 
    for i in range(10):
         temperature_journee=round(random.uniform(20,35),1)
@@ -32,12 +33,16 @@ print()
 #Question 2 : Croissance d'une population bactérienne
 
 def question2(pop_initiale):
+
+    print("Question 2 : Croissance d'une population bactérienne")
+    print()
     pop_initiale=float(input("Veuillez entrer la population initiale de bactéries."))
     pop_chaque_heure=[]
-    jour=1
+    jour=[]
     for i in range(11):
         pop_chaque_heure.append(pop_initiale)
         pop_initiale=pop_initiale*(np.pi/1.5)
+        jour.append(i)
 
     plt.figure(figsize=(10,10))
     plt.grid(True)
@@ -45,26 +50,10 @@ def question2(pop_initiale):
     plt.xlabel("Heure")
     plt.ylabel("Population")
     plt.plot([0,10],[50000,50000],"r--")
-    plt.plot(0, pop_chaque_heure[0], "*b")
-    plt.plot(1,pop_chaque_heure[1],"*b")
-    plt.plot(2, pop_chaque_heure[2], "*b")
-    plt.plot(3, pop_chaque_heure[3], "*b")
-    plt.plot(4, pop_chaque_heure[4], "*b")
-    plt.plot(5, pop_chaque_heure[5], "*b")
-    plt.plot(6, pop_chaque_heure[6], "*b")
-    plt.plot(7, pop_chaque_heure[7], "*b")
-    plt.plot(8, pop_chaque_heure[8], "*b")
-    plt.plot(9, pop_chaque_heure[9], "*b")
-    plt.plot(10, pop_chaque_heure[10], "*b")
+    plt.plot([jour],[pop_chaque_heure],"*b")
     plt.show()
 
-
 question2(100)
-
-
-
-
-
 
 
 
